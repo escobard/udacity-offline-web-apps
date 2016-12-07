66623751ccc5055e67ed3976e01b4074ce61e69f
Daniel Escobar <escobard90@gmail.com>
2016-12-06 17:42:57 -0700

    type: feat
    hijacked all .jpg requests and replaced them with the dr evil gif

--- lesson-2/wittr/public/js/sw/index.js
+++ lesson-2/wittr/public/js/sw/index.js
@@ -4,14 +4,20 @@ self.addEventListener('fetch', function(event) {
 	   console.log('Service Request');
 	  console.log(event.request);
 	  */
-	 event.respondWith(
+	 
+	 // this logs the data requests on page load
+	 console.log(event.request);
+
+	 // this loads the event response only if the requested url ends with .jpg
+	 if(event.request.url.endsWith('.jpg')) {
+	 	event.respondWith(
+		 	// respond with a fetch to a GIF
+		 	fetch('/imgs/dr-evil.gif')
+	 	); 
+	 }
 	 	/* this is an object
 	 	new Response('<strong class="a-winner-is-me">Hello <br>World<br></strong>', {
 	 	// the headers property takes an object of header's and values
 	 		headers: {'content-type':'text/html' }
-	 	}) */
-
-	 	// respond with a fetch to a GIF
-	 	fetch('/imgs/dr-evil.gif')
-	 	);
+	 	}) */	
 });

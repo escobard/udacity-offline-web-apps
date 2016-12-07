8374268d7350593826bb0d244fd2ecd840e952c7
Daniel Escobar <escobard90@gmail.com>
2016-12-06 17:21:36 -0700

    type: feat
    added fetch request to the service worker

--- lesson-2/wittr/public/js/sw/index.js
+++ lesson-2/wittr/public/js/sw/index.js
@@ -5,10 +5,13 @@ self.addEventListener('fetch', function(event) {
 	  console.log(event.request);
 	  */
 	 event.respondWith(
-	 	// this is an object
+	 	/* this is an object
 	 	new Response('<strong class="a-winner-is-me">Hello <br>World<br></strong>', {
 	 	// the headers property takes an object of header's and values
 	 		headers: {'content-type':'text/html' }
-	 	})
+	 	}) */
+
+	 	// respond with a fetch to a GIF
+	 	fetch('/imgs/dr-evil.gif')
 	 	);
 });

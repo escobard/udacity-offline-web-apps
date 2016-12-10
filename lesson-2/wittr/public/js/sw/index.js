dd2886b4027207cb298d1b64a7e809984b5b2b17
Daniel Escobar <escobard90@gmail.com>
2016-12-10 00:11:56 -0700

    type: feat
    added service worker cache delete, cache filter, and cache refactor to work scalably

--- lesson-2/wittr/public/js/sw/index.js
+++ lesson-2/wittr/public/js/sw/index.js
@@ -1,10 +1,15 @@
+// if versions are getting to a higher iteration number (say 19-20) it will get messy deleting caches since users might be on 14-17 and are
+// requesting #18.
+// To avoid complications, we create a variable to store the current cacche
+var staticCacheName = 'wittr-static-v2';
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
     // TODO: change the site's theme, eg swap the vars in public/scss/_theme.scss
     // Ensure at least $primary-color changes
     // TODO: change cache name to 'wittr-static-v2'
     // changed from witter-static-v1
-    caches.open('wittr-static-v2').then(function(cache) {
+    caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
         '/',
         'js/main.js',
@@ -20,8 +25,29 @@ self.addEventListener('install', function(event) {
 self.addEventListener('activate', function(event) {
 
   event.waitUntil(
+
+    // this grabs all of the cache names that currently exsist
+    caches.keys().then(function(cacheNames){
+    // wraps the filter, and the map promise in a Promise.all which waits until all the promises are completed to return the Promise.resolve
+    return Promise.all(
+      // creates a filter for the cache names after the caches have been requested
+      cacheNames.filter(function(cacheName){
+        // only return cache that returns with the set string
+        // this grabs a list of caches we don't need anymore, if the exsist, grabbing all caches except for our static cache
+        return cacheName.startWith('wittr-') && cacheName != staticCacheName;
+      })// this maps the caches returning a promise for each matching request, or grabs each individual cache in 
+      // the caches array and then does something with them
+      .map(function(cacheName){
+        // this deletes the caches that do not match the string in the cacheNames filter funciton, or the static cache
+        return cache.delete(cacheName);
+      })
+    );
+  })
+
   // this was grabbed from the instructor notes, deletes the old cache without any issues.   
-    return caches.delete('witter-static-v1')
+  // this is the quick solution, deprecating for scalable solution above
+  //  return caches.delete('witter-static-v1')
+
 
   // this was grabbed from : https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
   // Not working too well in our case, will review instructor quiz

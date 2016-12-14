932e269ce0abf458532604efa5e6e1fc27124e5e
Daniel Escobar <escobard90@gmail.com>
2016-12-13 19:57:33 -0700

    type: feat
    added code to upen the wittr db for the create db quiz

--- lesson-2/wittr/public/js/idb-test/index.js
+++ lesson-2/wittr/public/js/idb-test/index.js
@@ -1,240 +1,122 @@
 import idb from 'idb';
 
-// more on usage of this API here : https://github.com/jakearchibald/idb
-// goes as follows idb.open(name, version, upgradeCallback)
-// Example: 
-// This returns a promise, let's store that for later with a var
-
-var dbPromise = idb.open('test-db', 4, 
-  // this defines the database
-  function(upgradeDb){
-    
-    // this creates a switch to update the browser with the new version of the indexDB, to the new one
-    switch(upgradeDb.oldVersion){
-
-     // case 0 is called if the browser does not have the first version installed, then it installs it if it does not 
-      case 0:
-      // this store has a key that's separate to the data, which is what we want to store in keyvalStore
+var dbPromise = idb.open('test-db', 4, function(upgradeDb) {
+  switch(upgradeDb.oldVersion) {
+    case 0:
       var keyValStore = upgradeDb.createObjectStore('keyval');
-
-      //objectStore documentation here: https://developer.mozilla.org/en-US/docs/Web/API/IDBObjectStore
-      // accepts the values of objectStore(name, optionalKey)
-      keyValStore.put(
-        // this is the object name:
-        'world', 
-        // and this is the optional key
-        'hello');
-
-    // after case 1 is called, this stores  the new objectStore after the first case hase been creater, or if already created
-    // it installs case 1 after confirming case 0 is created
+      keyValStore.put("world", "hello");
     case 1:
-    
-      // object stores can only be called within the upgradeDb function nowhere else.
-      // below is the code  to create a new object store for the 'people' object
-      // This sets the object's 'name' as the key, which is stored by name rather than by key
-      // when adding new object stores, the version needs to be changed
-      upgradeDb.createObjectStore('people', {keyPath: 'name'});
-
-    // upgrading to a new version, to filter out DB results, by favorite animal.
-    // Must create a new case, created below
+      upgradeDb.createObjectStore('people', { keyPath: 'name' });
     case 2:
       var peopleStore = upgradeDb.transaction.objectStore('people');
-
-      // this creates an index, which sorts the 'vaforiteanimal' property
       peopleStore.createIndex('animal', 'favoriteAnimal');
-
-      // the code to list all the people will be modified to sort results by favoriteAnimal below
-    
-    // upgrading to version 4, for the quiz in lesson 3
     case 3:
-      var peopleStore = upgradeDb.transaction.objectStore('people');
-
+      peopleStore = upgradeDb.transaction.objectStore('people');
       peopleStore.createIndex('age', 'age');
-    }
-
-    
-
+  }
 });
 
-// this calls our database, and reads from it, after the promise has been fulfilled (dbPromise)
-dbPromise.then(function(db){
-
-  // this is the function to read from the database which accepts the objectStore which was created above, in this case keyval
-  // this function selects the 'keyval' object store, ready to use, with an optional 'do something' argument
-  var transaction = db.transaction('keyval');
-
-  // this calls the object store (DB) we want to display
-  var keyValDB = transaction.objectStore('keyval');
-
-  // this returns a key within the keyval object, in thie case the 'hello' created above
-  // this returns a promise
-  return keyValDB.get('hello');
-
-  // within the promise, we grab the value of hello, which in this case is world.
-  // the value of this key store is the function argument
-}).then(function(val){
+// read "hello" in "keyval"
+dbPromise.then(function(db) {
+  var tx = db.transaction('keyval');
+  var keyValStore = tx.objectStore('keyval');
+  return keyValStore.get('hello');
+}).then(function(val) {
   console.log('The value of "hello" is:', val);
 });
 
-// the following is used to add values to exsisting object stores
-dbPromise.then(function(db){
-  // once again grabs our DB, but this time, 'readwrite' is added to the optional valueto write something
-  var transaction = db.transaction('keyval', 'readwrite');
-
-  // this once again calls our db after its been registered
-  var keyValWrite = transaction.objectStore('keyval');
-
-  // this stores new values into ur DB, with the value being the first argument and the key being the second argument
-  keyValWrite.put('bar', 'foo');
-
-  // this returns a promise, that only returns if and when the transaction completes, and rejects if it fails
-  return transaction.complete;
-}).then(function(){
+// set "foo" to be "bar" in "keyval"
+dbPromise.then(function(db) {
+  var tx = db.transaction('keyval', 'readwrite');
+  var keyValStore = tx.objectStore('keyval');
+  keyValStore.put('bar', 'foo');
+  return tx.complete;
+}).then(function() {
   console.log('Added foo:bar to keyval');
 });
 
-// the following is used to add values to exsisting object stores
-dbPromise.then(function(db){
-  // once again grabs our DB, but this time, 'readwrite' is added to the optional valueto write something
-  var transaction = db.transaction('keyval', 'readwrite');
-
-  // this once again calls our db after its been registered
-  var keyValWrite = transaction.objectStore('keyval');
-
-  // this stores new values into ur DB, with the value being the first argument and the key being the second argument
-  keyValWrite.put('dog', 'favoriteAnimal');
-
-  // this returns a promise, that only returns if and when the transaction completes, and rejects if it fails
-  return transaction.complete;
-}).then(function(){
-  console.log('Added favoriteAnimal key : dog');
+dbPromise.then(function(db) {
+  var tx = db.transaction('keyval', 'readwrite');
+  var keyValStore = tx.objectStore('keyval');
+  keyValStore.put('cat', 'favoriteAnimal');
+  return tx.complete;
+}).then(function() {
+  console.log('Added favoriteAnimal:cat to keyval');
 });
 
-// the following creates the values to the people Object store:
-dbPromise.then(function(db){
-  // once again grabs our DB, but this time, 'readwrite' is added to the optional valueto write something
-  var transaction = db.transaction('people', 'readwrite');
+// add people to "people"
+dbPromise.then(function(db) {
+  var tx = db.transaction('people', 'readwrite');
+  var peopleStore = tx.objectStore('people');
 
-  // this once again calls our db after its been registered
-  var peopleStore = transaction.objectStore('people');
-
-  // this stores new values into ur DB, creating an object with whatever values I set within the transaction.put arguments.
-  // There is no key necessary here, since we have told this specific store to add the name property as the key for this DB
-  // Example here: 
   peopleStore.put({
-    name:'Sam Munoz',
+    name: 'Sam Munoz',
     age: 25,
     favoriteAnimal: 'dog'
   });
 
-  // here are more to people to add to this DB objectStore:
   peopleStore.put({
-    name:'Hermione Granger',
-    age: 18,
+    name: 'Susan Keller',
+    age: 34,
     favoriteAnimal: 'cat'
-  });  
+  });
 
   peopleStore.put({
-    name:'Harry Potter',
-    age: 19,
-    favoriteAnimal: 'owl'
-  });  
+    name: 'Lillie Wolfe',
+    age: 28,
+    favoriteAnimal: 'dog'
+  });
 
   peopleStore.put({
-    name:'Ronald Wesley',
-    age: 19,
-    favoriteAnimal: 'rat'
-  }); 
+    name: 'Marc Stone',
+    age: 39,
+    favoriteAnimal: 'cat'
+  });
 
-  // this returns a promise, that only returns if and when the transaction completes, and rejects if it fails
-  return transaction.complete;
-}).then(function(){
+  return tx.complete;
+}).then(function() {
   console.log('People added');
 });
 
-// the following reads the values of the people objectStore
-// this calls our database, and reads from it, after the promise has been fulfilled (dbPromise)
-dbPromise.then(function(db){
+// list all cat people
+dbPromise.then(function(db) {
+  var tx = db.transaction('people');
+  var peopleStore = tx.objectStore('people');
+  var animalIndex = peopleStore.index('animal');
 
-  // this is the function to read from the database which accepts the objectStore which was created above, in this case keyval
-  // this function selects the 'keyval' object store, ready to use, with an optional 'do something' argument
-  var transaction = db.transaction('people');
-
-  // this calls the object store (DB) we want to display
-  var peopleRead = transaction.objectStore('people');
-
-  // this calls the index we created for case 2 of this DB
-  var animalIndex = peopleRead.index('animal');
-
-  // this returns all keys and values within the people objectSTore
-  // return peopleRead.getAll();
-
-  // this returns the key values sorting them by the index we created for animals
-  // return animalIndex.getAll();
-
-  // this returns the key values of this objectStore that contain the getAll argument value.
-  // Example:
-  return animalIndex.getAll('cat')
-
-  // once fulfilled, this logs the values of the people store
-  // this grabs ALL the values of the DB and logs them in alphabetical order
-  // VERY IMPORTANT this is exactly how the values will be grabbed from the transportation API and placed within the view of the application
-  // 
-}).then(function(val){
-  console.log('People:', val);
+  return animalIndex.getAll('cat');
+}).then(function(people) {
+  console.log('Cat people:', people);
 });
 
-// index promise to grab the values of the 'age' index of the 'people' objectStore
-dbPromise.then(function(db){
-  var transaction = db.transaction('people');
-
-  // this calls the object store (DB) we want to display
-  var peopleRead = transaction.objectStore('people');
-
-  // this calls the index we created for case 2 of this DB
-  var ageIndex = peopleRead.index('age');
+// people by age
+dbPromise.then(function(db) {
+  var tx = db.transaction('people');
+  var peopleStore = tx.objectStore('people');
+  var ageIndex = peopleStore.index('age');
 
   return ageIndex.getAll();
-}).then(function(val){
-  console.log('Age', val);
+}).then(function(people) {
+  console.log('People by age:', people);
 });
 
-// the following can be used to create Cursors for index calues
-dbPromise.then(function(db){
-  var transaction = db.transaction('people');
+// Using cursors
+dbPromise.then(function(db) {
+  var tx = db.transaction('people');
+  var peopleStore = tx.objectStore('people');
+  var ageIndex = peopleStore.index('age');
 
-  var peopleRead = transaction.objectStore('people');
-
-  var ageIndex = peopleRead.index('age');
-
-  // this returns the cursos
   return ageIndex.openCursor();
-}).then(function logPerson(cursor){
-  // if the cursor value is null, then return
+}).then(function(cursor) {
   if (!cursor) return;
-  //
-  // cursor grabs values by object properties, the following grabs value and name
-  console.log('Cursored at:', cursor.value.name);
-
-  // returns the promise for the next cursor or leaves it undefined if there isnt a cursor
-  return cursor.continue()
-  // if cursor is valid, then it returns the next cursor value, or person in this case
-  // creates a loop that runs through all the values
-  .then(logPerson);
-
-  // other optional values for cursor are:
-  // cursor.update(newValue) - updates the current cursor with a value of our choice
-  // cursor.delete() - deletes the current cursor
-})
-
-// this can be used to skip cursor values, for example:
-.then(function(cursor){
-  if (!cursor) return;
-
-  // this skips the number of items specified in the function argument
   return cursor.advance(2);
-})
-.then(function(){
-  // this is only ran when the cursor has ran through all the DB values, and we are not at the end of the list
+}).then(function logPerson(cursor) {
+  if (!cursor) return;
+  console.log("Cursored at:", cursor.value.name);
+  // I could also do things like:
+  // cursor.update(newValue) to change the value, or
+  // cursor.delete() to delete this entry
+  return cursor.continue().then(logPerson);
+}).then(function() {
   console.log('Done cursoring');
 });
--- lesson-2/wittr/public/js/sw/index.js
+++ lesson-2/wittr/public/js/sw/index.js
@@ -1,12 +1,9 @@
 var staticCacheName = 'wittr-static-v4';
 
 self.addEventListener('install', function(event) {
-  // TODO: cache /skeleton rather than the root page
-
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
-        // changing from root page (/) to skeleton. not to clear yet on what this is exactly
         '/skeleton',
         'js/main.js',
         'css/main.css',
@@ -34,19 +31,11 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  // TODO: respond to requests for the root page with
-  // the page skeleton from the cache
-  // parses the request URL form the cache
-  var requestURL = new URL(event.request.url);
+  var requestUrl = new URL(event.request.url);
 
-  // checks to see if the request URL is the same as the current origin
-  if (requestURL.origin === location.origin) {
-    //after the first request is true, checks if the user's request URL is the root
-    if (requestURL.pathname ==='/') {
-      // and if so, responds with the /skeleton URL instead
-      // grabs the /skeleton URL straight, from the cache
+  if (requestUrl.origin === location.origin) {
+    if (requestUrl.pathname === '/') {
       event.respondWith(caches.match('/skeleton'));
-      // no need to go the network as a fallback if this fails, as /skeleton is cached no matter what during the sw install
       return;
     }
   }
@@ -63,4 +52,3 @@ self.addEventListener('message', function(event) {
     self.skipWaiting();
   }
 });
-///adsfa
